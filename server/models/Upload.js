cc964c0b081d46308b7dfee1aee7ff410f68e9bd
Yoona jeong <yoonalove77@naver.com>
2025-08-14 13:37:25 +0900

    [feat]repo clone and make server folder

---
+++ server/models/Upload.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const uploadSchema = mongoose.Schema({
+    
+})
+
+const Upload = mongoose.model('Upload', uploadSchema);
+
+module.exports = { Upload }
