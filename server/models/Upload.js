aba40ffbe835c6dfabe64ee2ceb838f290016551
Yoona jeong <yoonalove77@naver.com>
2025-08-19 20:24:09 +0900

    [test] Deployment Testing: Excluding BE+DB

--- server/models/Upload.js
+++ server/models/Upload.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-const uploadSchema = mongoose.Schema({
+// const uploadSchema = mongoose.Schema({
     
-})
+// })
 
-const Upload = mongoose.model('Upload', uploadSchema);
+// const Upload = mongoose.model('Upload', uploadSchema);
 
-module.exports = { Upload }
+// module.exports = { Upload }
