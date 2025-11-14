c86bfee1a217d5d962f96f793ff08bf5a9f54f7d
Yoona jeong <yoonalove77@naver.com>
2025-11-14 20:34:24 +0900

    [add] add student info_251114

--- client/src/pages/Students.jsx
+++ client/src/pages/Students.jsx
@@ -3,13 +3,18 @@ import { GithubOutlined, IdcardOutlined } from '@ant-design/icons';
 import phd_img_1 from '../assets/phd_img_1.jpg';
 import phd_img_2 from '../assets/phd_img_2.jpg';
 import phd_img_3 from '../assets/phd_img_3.jpg';
+import phd_img_4 from '../assets/phd_img_4.jpg';
+import phd_img_5 from '../assets/phd_img_5.jpg';
+// import phd_img_6 from '../assets/phd_img_6.jpg';
 import ms_img_1 from '../assets/ms_img_1.jpg';
 import ms_img_2 from '../assets/ms_img_2.jpg';
-import under_img_1 from '../assets/under_img_1.jpg';
-import under_img_2 from '../assets/under_img_2.jpg';
-import under_img_3 from '../assets/under_img_3.jpg';
-import under_img_4 from '../assets/under_img_4.jpg';
-import under_img_5 from '../assets/under_img_5.png';
+import ms_img_3 from '../assets/ms_img_3.jpg';
+import ms_img_4 from '../assets/ms_img_4.jpg';
+import ms_img_5 from '../assets/ms_img_5.jpg';
+import ms_img_6 from '../assets/ms_img_6.jpg';
+import ms_img_7 from '../assets/ms_img_7.jpg';
+import ms_img_8 from '../assets/ms_img_8.png';
+
 import '../styles/Members.css';
 
 export default function Students() {
@@ -78,14 +83,60 @@ export default function Students() {
                   <h1 className="member-name">Hosoo Shin (신호수)</h1>
                   <div className="info-switch-container">
                     <p className="email">enphilo@hanyang.ac.kr</p>
-                    <p className="student-research-keyword"></p>
-                    <p className="student-research-focus"></p>
+                    <p className="student-research-keyword">Solutions for Medical Intelligence</p>
+                    <p className="student-research-focus">Robust Diagnostics, AI Applications</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="student-profile">
+                <div className="profile-image-container">
+                  <img src={phd_img_4} alt="LiQiu Suo" className="profile-image" />
+                </div>
+                
+                <div className="profile-info">
+                  <h1 className="member-name">Liqiu Suo (소리추)</h1>
+                  <div className="info-switch-container">
+                    <p className="email">soda0808@hanyang.ac.kr</p>
+                    <p className="student-research-keyword">Solutions for E-commerce behavior</p>
+                    <p className="student-research-focus">AI Applications</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="student-profile">
+                <div className="profile-image-container">
+                  <img src={phd_img_5} alt="Jaemin Yang" className="profile-image" />
+                </div>
+                
+                <div className="profile-info">
+                  <h1 className="member-name">Jaemin Yang (양재민)</h1>
+                  <div className="info-switch-container">
+                    <p className="email">jaemin3404@hanyang.ac.kr </p>
+                    <p className="student-research-keyword">Solutions for Financial Intelligence</p>
+                    <p className="student-research-focus">Multi-Modal and Task Learning, AI Applications</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="student-profile">
+                <div className="profile-image-container">
+                  {/* <img src={phd_img_6} alt="BitChan Eom" className="profile-image" /> */}
+                </div>
+                
+                <div className="profile-info">
+                  <h1 className="member-name">Bitchan Eom (엄빛찬)</h1>
+                  <div className="info-switch-container">
+                    <p className="email">ubc445@naver.com</p>
+                    <p className="student-research-keyword">Solutions for Network Systems</p>
+                    <p className="student-research-focus">Network Optimization, AI Applications</p>
                   </div>
                 </div>
               </div>
 
             </div>
           </div>
+              
 
           {/* Master's Program */}
           <div className="student-category">
@@ -131,16 +182,25 @@ export default function Students() {
                   </a>
                 </div>
               </div>
-            </div>
-          </div>
 
-          {/* Undergraduate Intern */}
-          <div className="student-category">
-            <h3 className="category-title">Undergraduate Intern</h3>
-            <div className="students-grid">
               <div className="student-profile">
                 <div className="profile-image-container">
-                  <img src={under_img_1} alt="Gibyung Kang" className="profile-image" />
+                  <img src={ms_img_3} alt="harim" className="profile-image" />
+                </div>
+                
+                <div className="profile-info">
+                  <h1 className="member-name">Xia Lin (하림)</h1>
+                  <div className="info-switch-container">
+                    <p className="email">harim1002@hanyang.ac.kr</p>
+                    <p className="student-research-keyword"></p>
+                    <p className="student-research-focus"></p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="student-profile">
+                <div className="profile-image-container">
+                  <img src={ms_img_4} alt="Gibyung Kang" className="profile-image" />
                 </div>
                 
                 <div className="profile-info">
@@ -161,7 +221,7 @@ export default function Students() {
 
               <div className="student-profile">
                 <div className="profile-image-container">
-                  <img src={under_img_2} alt="Seungo Kwon" className="profile-image" />
+                  <img src={ms_img_5} alt="Seungo Kwon" className="profile-image" />
                 </div>
                 
                 <div className="profile-info">
@@ -182,51 +242,59 @@ export default function Students() {
 
               <div className="student-profile">
                 <div className="profile-image-container">
-                  <img src={under_img_3} alt="Hosoo Shin" className="profile-image" />
+                  <img src={ms_img_6} alt="Hosoo Shin" className="profile-image" />
                 </div>
                 
                 <div className="profile-info">
                   <h1 className="member-name">Seongyeon Son (손성연)</h1>
                   <div className="info-switch-container">
                     <p className="email">annssy@hanyang.ac.kr</p>
-                    <p className="student-research-keyword"></p>
-                    <p className="student-research-focus"></p>
+                    <p className="student-research-keyword">Solutions for Sustainable Data Intelligence</p>
+                    <p className="student-research-focus">Environmental Forecasting, AI Applications</p>
                   </div>
                 </div>
               </div>
 
               <div className="student-profile">
                 <div className="profile-image-container">
-                  <img src={under_img_4} alt="Hosoo Shin" className="profile-image" />
+                  <img src={ms_img_7} alt="Hosoo Shin" className="profile-image" />
                 </div>
                 
                 <div className="profile-info">
                   <h1 className="member-name">Byeongchan Go (고병찬)</h1>
                   <div className="info-switch-container">
-                    <p className="email">@hanyang.ac.kr</p>
-                    <p className="student-research-keyword"></p>
-                    <p className="student-research-focus"></p>
+                    <p className="email">bcgo99@hanyang.ac.kr</p>
+                    <p className="student-research-keyword">Solutions for Platform Design</p>
+                    <p className="student-research-focus">Behavior Modeling, AI Applications</p>
                   </div>
                 </div>
               </div>
 
               <div className="student-profile">
                 <div className="profile-image-container">
-                  <img src={under_img_5} alt="Hosoo Shin" className="profile-image" />
+                  <img src={ms_img_8} alt="Hosoo Shin" className="profile-image" />
                 </div>
                 
                 <div className="profile-info">
                   <h1 className="member-name">Minhee Park (박민희)</h1>
                   <div className="info-switch-container">
                     <p className="email">bagminhui927@gmail.com</p>
-                    <p className="student-research-keyword"></p>
-                    <p className="student-research-focus"></p>
+                    <p className="student-research-keyword">Solutions for Data-Driven Techniques</p>
+                    <p className="student-research-focus">Data Intelligence, AI Applications</p>
                   </div>
                 </div>
               </div>
 
             </div>
           </div>
+
+          {/* Undergraduate Intern */}
+          {/* <div className="student-category">
+            <h3 className="category-title">Undergraduate Intern</h3>
+            <div className="students-grid">
+
+            </div>
+          </div> */}
         </div>
       </div>
     </div>
